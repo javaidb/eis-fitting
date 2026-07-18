96bf327202e63a5bf6bd51abd7d8db16dcc4d12e
Javaid Baksh <javaidbaksh@gmail.com>
2026-07-18 19:44:43 -0400

    feat: update navigation for back button to return to fitting screen

--- static/js/views/trends.js
+++ static/js/views/trends.js
@@ -14,7 +14,7 @@ export function TrendsView(container, { navigate, showToast }) {
           <div>Run fitting first to see parameter trends.</div>
           <button class="btn btn-secondary" style="margin-top:12px;" id="back-btn">← Back to Fitting</button>
         </div>`;
-      container.querySelector('#back-btn')?.addEventListener('click', () => navigate(6));
+      container.querySelector('#back-btn')?.addEventListener('click', () => navigate(4));
       return;
     }
 
@@ -179,7 +179,7 @@ export function TrendsView(container, { navigate, showToast }) {
 
     updateBtn.addEventListener('click', updatePlots);
     exportBtn.addEventListener('click', exportCSV);
-    backBtn.addEventListener('click', () => navigate(6));
+    backBtn.addEventListener('click', () => navigate(4));
 
     updatePlots();
 
